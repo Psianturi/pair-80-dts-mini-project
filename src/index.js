5a9104aff5c077acbb6495491ab4c8367d8ba951
nikolaijan <posma2018@gmail.com>
2022-07-17 12:33:56 +0700

    Commit dev

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-i1w56i2y.us.auth0.com"
+    clientId="kFnJaObHaly1YSf9QHmgFcddwUeMqhXV"
+    redirectUri={window.location.origin}
+  >
+    
+  </Auth0Provider>
     <App />
   </React.StrictMode>
 );
