ae0d4702e64a8fcd4947114c0b7a2efbff48fafc
igo <igoanggaraa@gmail.com>
2022-07-20 09:35:31 +0700

    finall

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,20 +4,11 @@ import { Segment } from "semantic-ui-react";
 const Footer = () => {
   return (
     <Segment inverted color="blue" textAlign="center">
-      MINI PROJECT PAIR-80 DTSREA2A &#169;2022{" "}
-    </Segment>
-  );
-};
-
-export default Footer;
-=======
-      <h3>MINI PROJECT PAIR-80 DTSREA2A &#169;2022{" "} </h3>
+      <h3>MINI PROJECT PAIR-80 DTSREA2A &#169;2022 </h3>
       <br></br>
-      https://github.com/Psianturi{" "}
-      <br></br>
+      https://github.com/Psianturi <br></br>
       https://github.com/anggaraigo{" "}
     </Segment>
-    
   );
 };
 
